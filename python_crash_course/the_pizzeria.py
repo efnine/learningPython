5b8cf7f763e8bccd14f6cb363d5b63ea4fc2d144
danie <danie@192.168.2.235>
2018-01-27 14:54:32 -0500

    DN - update the pizzeria

--- python_crash_course/the_pizzeria.py
+++ python_crash_course/the_pizzeria.py
@@ -2,19 +2,21 @@
 def makePizza(number_of_toppings):
     customer_toppings = []
     toppings_count = 0
-    for i in range(0,number_of_toppings):
-        available_toppings = ['mushrooms','cheese','chicken','pianeapple','bacon']
+    available_toppings = ['mushrooms','cheese','chicken','pianeapple','bacon']
+    
+    # sometimes a while loop is better than a for loop
+    while toppings_count < number_of_toppings:
         costumer_topping = input("Which topping would you like in your pizza? ")
+        
         if costumer_topping in available_toppings:
+            print("Perfect! I will add " + costumer_topping.title() + " to your pizza\n")
+            customer_toppings.append(costumer_topping)
             toppings_count = toppings_count + 1
-            if toppings_count < number_of_toppings:
-                print("Perfect! I will add " + costumer_topping.title() + " to your pizza\n")
-                customer_toppings.append(costumer_topping)
         else:
             print("Sorry, we do not have " + costumer_topping.title() + " as a featured topping.\n")
-                
-            
-    print("Today we will make a pizza with: " + str(customer_toppings))
+    
+    print("\nToday we will make a pizza with: " + str(customer_toppings))
+    
     
 makePizza(2)
 
