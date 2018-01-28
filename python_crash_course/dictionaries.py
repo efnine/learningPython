da063511a079338408187d39dbba83d65d8aad94
danie <danie@192.168.2.235>
2018-01-28 18:24:34 -0500

    DN - started dictionary lesson. pg 97

---
+++ python_crash_course/dictionaries.py
@@ -0,0 +1,8 @@
+alien_0 = {'color':'green','points':5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+print('\n')
+
+new_points = alien_0['points']
+print("You have accumulated a total of " + str(new_points) + " points!")
