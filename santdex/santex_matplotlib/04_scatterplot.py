39e60c154082e6ffda0a1406772109baa6471958
danie <danie@192.168.2.235>
2018-02-15 22:03:18 -0500

    dn - created a scatter plot

---
+++ santdex/santex_matplotlib/04_scatterplot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+### scatter
+x = [1,2,3,4,5,6,7,8]
+y = [5,2,4,3,4,5,8,9]
+
+plt.scatter(x,y, label='skitscat', color='k', s=10)
+
+plt.xlabel('Plot Number')
+plt.ylabel('Important Variable')
+plt.title('Interesting Title\nCheck Out Subtitle')
+plt.legend()
+
+plt.show()
